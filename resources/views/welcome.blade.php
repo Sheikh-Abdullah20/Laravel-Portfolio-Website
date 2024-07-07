35bec4d5b75cac7cdac4b87216384c555f1a6fa1
Sheikh-Abdullah20 <162795515+Sheikh-Abdullah20@users.noreply.github.com>
2024-07-07 23:59:44 +0500

    card fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,67 +25,66 @@ Home
 
 
     <div class="row text-center my-5">
-        <h2 class="display-4">Services</h2>
+        <h2 class="display-1">Services</h2>
         <hr>
     </div>
 
     <div class="row ">
-        <div class="col-md-6 d-flex justify-content-center my-2">
+        <div class="col-md-4 d-flex justify-content-center my-2">
             <div class="card  bg-dark text-light" style="width: 25rem; height: auto" >
                 <img src="images/full stack.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="display-6">Full Stack Development</h5>
                   <p class="card-text">Full stack development refers to the development of both the front-end and back-end portions of a web application. A full stack developer has the skills to work on the entire stack of technology, from the user interface to the database and server-side logic.</p>
                  <hr>
-                 <div class="row">
-                    <div class="col-md-12 my-1">
+                </div>
+                <div class="row">
+                    <div class="col-md-12 my-1 d-flex justify-content-center mb-2">
                         <a href='{{route('contact')}}' class="btn btn-outline-info w-50">Contact Us</a>
                     </div>
                  </div>
-                </div>
               </div>
         </div>
 
-        <div class="col-md-6 d-flex justify-content-center my-2">
+        <div class="col-md-4 d-flex justify-content-center my-2">
             <div class="card bg-dark text-light" style="width: 25rem; height: auto" >
                 <img src="images/figma.webp" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="display-6">Ui & Ux</h5>
                   <p class="card-text">UI/UX design focuses on the user interface and user experience of digital products. UI (User Interface) design is about creating visually appealing and intuitive interfaces, while UX (User Experience) design is about enhancing user satisfaction by improving the usability and accessibility of a product. </p>
                 <hr>
+               
+                </div>
                 <div class="row ">
-                    <div class="col-md-12 ">
+                    <div class="col-md-12 d-flex justify-content-center mb-2">
                         <a href='{{route('contact')}}' class="btn btn-outline-info w-50">Contact Us</a>
                     </div>
-                </div>
                 </div>
               </div>
         </div>
 
-    </div>
-
-    <div class="row">
-        <div class="col-md-12 d-flex justify-content-center my-2">
+        <div class="col-md-4 d-flex justify-content-center my-2">
             <div class="card  bg-dark text-light" style="width: 25rem; height: auto" >
                 <img src="images/web development.jpg" class="card-img-top" alt="..." height="180px">
                 <div class="card-body">
                   <h5 class="display-6">Web Development</h5>
                   <p class="card-text">Web development is the process of creating and maintaining websites. It involves a range of tasks including web design, web content creation, client-side/server-side scripting, and network security configuration. Web developers use various programming languages and frameworks to build responsive, The field is constantly evolving with new technologies and tools</p>
                <hr>
-               <div class="row">
-                <div class="col-md-12 ">
-                    <a href='{{route('contact')}}' class="btn btn-outline-info w-50">Contact Us</a>
                 </div>
-               </div>
-                </div>
+                <div class="row">
+                    <div class="col-md-12 d-flex justify-content-center mb-2 ">
+                        <a href='{{route('contact')}}' class="btn btn-outline-info w-50">Contact Us</a>
+                    </div>
+                   </div>
               </div>
         </div>
+
     </div>
 
     <hr>
 
     <div class="row text-center my-5">
-        <h2 class="display-5">Want to Contact..?</h2>
+        <h2 class="display-1">Want to Contact..?</h2>
     </div>
 
     <div class="row align-items-center">
