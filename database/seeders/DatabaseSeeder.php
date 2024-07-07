71a1e0e26224550cfbe67edcae7247867c8acfdc
Sheikh-Abdullah20 <162795515+Sheikh-Abdullah20@users.noreply.github.com>
2024-07-07 12:56:40 +0500

    DataBase Connectivity Remaining

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
+    { 
+        
        $this->call([
         userSeeder::class
        ]);
